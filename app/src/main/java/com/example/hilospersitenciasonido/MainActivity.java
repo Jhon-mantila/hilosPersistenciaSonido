a7c16da61f51d2ca05a345c03f78d8eedf592c8b
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-08-14 20:53:41 -0500

    intent del boton ayuda

--- app/src/main/java/com/example/hilospersitenciasonido/MainActivity.java
+++ app/src/main/java/com/example/hilospersitenciasonido/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hilospersitenciasonido;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ayuda(View vista){
+
+        Intent intencion = new Intent(this, AyudaActividad.class);
+
+        startActivity(intencion);
+
     }
 }
